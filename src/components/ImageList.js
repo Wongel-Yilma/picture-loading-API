d22c2017236b5a77e20761989bbfdb96f0f9a344
Wongelemengist Yilma <wongel.yilma2016@gmail.com>
2022-08-21 16:20:51 +0300

    Implemented the automatic span on image height

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./ImageList.css";
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
   console.log(props);
-  const images = props.images.map(({ description, id, urls }) => (
-    <img alt={description} key={id} src={urls.regular}></img>
+  const images = props.images.map((image) => (
+    <ImageCard key={image.id} image={image} />
   ));
   return <div className="image-list">{images}</div>;
 };
